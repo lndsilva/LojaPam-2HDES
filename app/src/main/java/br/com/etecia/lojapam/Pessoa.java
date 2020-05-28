7fac3fd0babb25a47c990cc2d7c2b372949c4747
Laercio-pc <lndsilva@hotmail.com>
2020-05-27 22:31:16 -0300

    CardView e ScrollView

--- app/src/main/java/br/com/etecia/lojapam/Pessoa.java
+++ app/src/main/java/br/com/etecia/lojapam/Pessoa.java
@@ -4,14 +4,19 @@ public class Pessoa {
 
     private String nome, email, telefone;
 
+    //alt+insert ou clicar com o botão direito do mouse e
+    //selecionar o item Generate do menu suspenso
+
+    //Construtor vazio
     public Pessoa() {
     }
-
+    //Construtor com parâmetros
     public Pessoa(String nome, String email, String telefone) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
     }
+    //Criado os getters e setters
 
     public String getNome() {
         return nome;
