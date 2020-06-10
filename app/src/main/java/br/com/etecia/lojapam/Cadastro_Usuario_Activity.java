f7e44bad88cc4cd7aa7713e1aa447ecbe45fa736
Laercio-pc <lndsilva@hotmail.com>
2020-06-10 19:16:44 -0300

    validação de editText vazio

--- app/src/main/java/br/com/etecia/lojapam/Cadastro_Usuario_Activity.java
+++ app/src/main/java/br/com/etecia/lojapam/Cadastro_Usuario_Activity.java
@@ -4,14 +4,86 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cadastro_Usuario_Activity extends AppCompatActivity {
+    Button btnCadUsu;
+    EditText txtCadUsuNome, txtCadUsuTel, txtCadUsuCPF, txtCadUsuEmail,
+            txtCadUsuSenha, txtCadUsuCSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro__usuario_layout);
+
+        btnCadUsu = findViewById(R.id.btnCadUsuConfirmar);
+        txtCadUsuNome = findViewById(R.id.txtCadUsuNome);
+        txtCadUsuCPF = findViewById(R.id.txtCadUsuCPF);
+        txtCadUsuTel = findViewById(R.id.txtCadUsuTel);
+        txtCadUsuSenha = findViewById(R.id.txtCadUsuSenha);
+        txtCadUsuCSenha = findViewById(R.id.txtCadUsuConfSenha);
+        txtCadUsuEmail = findViewById(R.id.txtCadUsuEmail);
+
+        btnCadUsu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nomeUsu = txtCadUsuNome.getText().toString();
+                String cpfUsu = txtCadUsuCPF.getText().toString();
+                String telUsu = txtCadUsuTel.getText().toString();
+                String emailUsu = txtCadUsuEmail.getText().toString();
+                String senhaUsu = txtCadUsuSenha.getText().toString();
+                String csenhaUsu = txtCadUsuCSenha.getText().toString();
+
+                //Verifica se o campo está vazio
+                if (nomeUsu.isEmpty()) {
+                    txtCadUsuNome.setError("Entre com o nome");
+                    txtCadUsuNome.requestFocus();
+                }
+                if (cpfUsu.isEmpty()) {
+                    txtCadUsuCPF.setError("Entre com o CPF");
+                    txtCadUsuCPF.requestFocus();
+                }
+                if (telUsu.isEmpty()) {
+                    txtCadUsuTel.setError("Entre com o CPF");
+                    txtCadUsuTel.requestFocus();
+                }
+                if (emailUsu.isEmpty()) {
+                    txtCadUsuEmail.setError("Entre com o CPF");
+                    txtCadUsuEmail.requestFocus();
+                }
+                if (senhaUsu.isEmpty()) {
+                    txtCadUsuSenha.setError("Entre com o CPF");
+                    txtCadUsuSenha.requestFocus();
+                }
+                if (csenhaUsu.isEmpty()) {
+                    txtCadUsuCSenha.setError("Entre com o CPF");
+                    txtCadUsuCSenha.requestFocus();
+                }
+
+                //Limpa os campos apos o cadastro
+                txtCadUsuNome.setText("");
+                txtCadUsuEmail.setText("");
+                txtCadUsuTel.setText("");
+                txtCadUsuCPF.setText("");
+                txtCadUsuSenha.setText("");
+                txtCadUsuCSenha.setText("");
+                //Coloca o cursor no campo nome
+                txtCadUsuNome.requestFocus();
+
+                Toast.makeText(getApplicationContext(),
+                        "Usuário inserido com sucesso...",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     //Inserindo ações na seta de retorno do smartphone
