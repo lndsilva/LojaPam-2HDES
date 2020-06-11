c734b7f0777b3069dd5c0cb7df8b005abb0efc91
Laercio-pc <lndsilva@hotmail.com>
2020-06-10 22:33:38 -0300

    Layouts

---
+++ app/src/main/java/br/com/etecia/lojapam/Coordinator_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.lojapam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Coordinator_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coordinator_layout);
+    }
+}
