a2c2f0863e62f34a33dc4e562c530e47ab821ef1
Laercio-pc <lndsilva@hotmail.com>
2020-06-10 21:01:03 -0300

    componentes material design

---
+++ app/src/main/java/br/com/etecia/lojapam/Testa_Componentes_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.lojapam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Testa_Componentes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testa__componentes_layout);
+    }
+}
